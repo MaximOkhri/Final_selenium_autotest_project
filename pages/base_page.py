6789c2a64229dd64ce084487255cbaa13d3e99b9
unknown <okhri932010@gmail.com>
2023-04-12 19:34:07 +0200

    изменение в файле test_main_page.py и добавление новой папки pages с файлами base_page.py и main_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+class BasePage():
+    def __init__(self, browser: WebDriver, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,15 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser: WebDriver):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser: WebDriver):
+
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser: WebDriver):
+    
+    browser.get(link)
+    go_to_login_page(browser)
