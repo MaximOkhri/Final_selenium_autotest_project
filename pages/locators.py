069648e589a07fb8426d1d342af05fe6ea73c271
unknown <okhri932010@gmail.com>
2023-04-15 19:59:53 +0200

    Создание негативных проверок и изменение проверки login link

--- pages/locators.py
+++ pages/locators.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -11,5 +12,5 @@ class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
     PRODUCT_NAME = (By.TAG_NAME, "h1")
     PRODUCT_PRICE = (By.XPATH, "//*[@id='content_inner']/article/div[1]/div[2]/p[1]")
-    NAME_MESSEGE = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
-    PRICE_MESSEGE = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
+    SUCCESS_MESSAGE = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+    PRICE_MESSAGE = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
--- pages/product_page.py
+++ pages/product_page.py
@@ -10,14 +10,20 @@ class ProductPage(BasePage):
     def should_be_add_to_basket_messege(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         product_name_text = product_name.text
-        name_massege = self.browser.find_element(*ProductPageLocators.NAME_MESSEGE)
-        name_massege_text = name_massege.text
-        assert product_name_text == name_massege_text, "product names are different"
+        name_massage = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE)
+        name_massage_text = name_massage.text
+        assert product_name_text == name_massage_text, "product names are different"
 
 
     def should_be_right_basket_price(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
         product_price_text = product_price.text
-        price_messege = self.browser.find_element(*ProductPageLocators.PRICE_MESSEGE)
-        price_messege_text = price_messege.text
-        assert product_price_text == price_messege_text, "product prices are different"
+        price_message = self.browser.find_element(*ProductPageLocators.PRICE_MESSAGE)
+        price_message_text = price_message.text
+        assert product_price_text == price_message_text, "product prices are different"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not disappeared, but should be"
